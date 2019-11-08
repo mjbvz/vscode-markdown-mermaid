fe65ac227eaeeeaf289473f24efbdf1edd76c46e
foamy-latte <35882542+winterhotlatte@users.noreply.github.com>
2019-11-08 14:28:56 +0900

    Fix: Class annotations

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ module.exports.activate = () => {
         extendMarkdownIt(md) {
             const highlight = md.options.highlight;
             md.options.highlight = (code, lang) => {
+                /* Class annotation doesn't work
+                https://github.com/mjbvz/vscode-markdown-mermaid/issues/39 */
+                if (code.includes("classDiagram")) {
+                    code = code.replace(/<<[A-Za-z].*>>/g, "");
+                }
+
                 if (lang && lang.match(/\bmermaid\b/i)) {
                     return `<div class="mermaid">${code}</div>`;
                 }
