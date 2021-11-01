7409921fa3ef2d65eaa59cca86a6c4f47ddbb1a3
Matt Bierner <matb@microsoft.com>
2021-11-01 16:14:05 -0700

    Add support for VS Code's new diff based md renderer

--- page.js
+++ page.js
@@ -1,8 +1,40 @@
+//@ts-check
 import mermaid from 'mermaid';
 
-mermaid.initialize({
-    startOnLoad: true,
-    theme: document.body.classList.contains('vscode-dark') || document.body.classList.contains('vscode-high-contrast')
-        ? 'dark'
-        : 'default'
-});
+function init() {
+    mermaid.initialize({
+        startOnLoad: false,
+        theme: document.body.classList.contains('vscode-dark') || document.body.classList.contains('vscode-high-contrast')
+            ? 'dark'
+            : 'default'
+    });
+
+    // Delete existing mermaid outputs
+    for (const possibleMermaidErrorOut of document.getElementsByTagName('svg')) {
+        const parent = possibleMermaidErrorOut.parentElement;
+        if (parent && parent.id.startsWith('dmermaid')) {
+            parent.remove();
+        }
+    }
+
+    let i = 0;
+    for (const mermaidContainer of document.getElementsByClassName('mermaid')) {
+        const id = `mermaid-${Date.now()}-${i++}`;
+        const source = mermaidContainer.textContent;
+
+        const out = document.createElement('div');
+        out.id = id;
+        mermaidContainer.innerHTML = '';
+        mermaidContainer.appendChild(out);
+
+        mermaid.render(id, source, (out) => {
+            console.log(out);
+            mermaidContainer.innerHTML = out;
+        });
+    }
+}
+
+
+window.addEventListener('vscode.markdown.updateContent', init);
+
+init();
