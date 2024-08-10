3010999436ad9c942d865bfca1865c80779fdfc4
snopan <snopan@protonmail.com>
2024-08-11 00:24:11 +1000

    Added zoom capabilities based on default dimensions

--- markdownPreview/index.ts
+++ markdownPreview/index.ts
@@ -1,5 +1,6 @@
 import mermaid from 'mermaid';
 import { renderMermaidBlocksInElement } from './mermaid';
+import svgPanZoom from 'svg-pan-zoom';
 
 function init() {
     const configSpan = document.getElementById('markdown-mermaid');
@@ -16,6 +17,20 @@ function init() {
 
     renderMermaidBlocksInElement(document.body, (mermaidContainer, content) => {
         mermaidContainer.innerHTML = content;
+        const svgEl = mermaidContainer.querySelector("svg");
+        if (!svgEl) return
+
+        // Explicitly set svg element to it's default dimension
+        // so svg-pan-zoom can setup correctly
+        var bBox = svgEl.getBBox();
+        svgEl.style.width = bBox.width+"px";
+        svgEl.style.height = bBox.height+"px";
+        svgPanZoom(svgEl, {
+            zoomEnabled: true,
+            controlIconsEnabled: true,
+            fit: true,
+            center: true
+        }); 
     });
 }
 
