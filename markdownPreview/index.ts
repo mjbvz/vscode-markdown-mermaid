51ac34bc07823ffc677d5daaf0f919f93c71d3f6
snopan <snopan@protonmail.com>
2024-08-14 09:36:33 +1000

    Added button for enable and disable zoom

--- markdownPreview/index.ts
+++ markdownPreview/index.ts
@@ -17,23 +17,39 @@ function init() {
 
     renderMermaidBlocksInElement(document.body, (mermaidContainer, content) => {
         mermaidContainer.innerHTML = content;
+        mermaidContainer.style.display = "flex"
+        mermaidContainer.style.flexDirection = "column"
+        
+        const button = document.createElement("BUTTON")
+        button.innerText = "Enable Zoom"
+        button.style.width = "10rem"
+        button.onclick = () => {onClickZoom(button, mermaidContainer, content)}
+        mermaidContainer.prepend(button);
+    });
+}
+
+function onClickZoom(button: HTMLElement, mermaidContainer: HTMLElement, svgContent: string) {
+    if (button.innerText == "Enable Zoom") {
         const svgEl = mermaidContainer.querySelector("svg");
         if (!svgEl) return
-
-        // Explicitly set svg element to it's default dimension
-        // so svg-pan-zoom can setup correctly
-        var bBox = svgEl.getBBox();
-        svgEl.style.width = bBox.width+"px";
-        svgEl.style.height = bBox.height+"px";
+    
+        const svgSize = svgEl.getBoundingClientRect()
+        svgEl.style.width = svgSize.width+"px";
+        svgEl.style.height = svgSize.height+"px";
         svgPanZoom(svgEl, {
             zoomEnabled: true,
             controlIconsEnabled: true,
             fit: true,
             center: true
         }); 
-    });
-}
-
+        button.innerText = "Disable Zoom"
+    }
+    else {
+        mermaidContainer.innerHTML = svgContent;
+        mermaidContainer.prepend(button);
+        button.innerText = "Enable Zoom"
+    }
+} 
 
 window.addEventListener('vscode.markdown.updateContent', init);
 
