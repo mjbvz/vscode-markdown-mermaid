b349276c34445712bfdfb369d9f2238fcd15f380
snopan <snopan@protonmail.com>
2024-08-14 13:37:51 +1000

    Refactor zoom related code to another file

--- src/markdownPreview/index.ts
+++ src/markdownPreview/index.ts
@@ -5,7 +5,7 @@
  */
 import mermaid, { MermaidConfig } from 'mermaid';
 import { registerMermaidAddons, renderMermaidBlocksInElement } from '../shared-mermaid';
-import svgPanZoom from 'svg-pan-zoom';
+import { createZoomButton, resetView } from './zoom';
 
 function init() { 
     const configSpan = document.getElementById('markdown-mermaid');
@@ -29,45 +29,10 @@ function init() {
         mermaidContainer.style.display = "flex"
         mermaidContainer.style.flexDirection = "column"
         
-        // Create button, setup style and add click listener
-        const button = document.createElement("BUTTON")
-        button.style.width = "10rem"
-        button.onclick = () => {onClickZoom(button, mermaidContainer, content)}
-
-        resetView(button, mermaidContainer, content)
+        // Set the view to be normal svg diagram
+        const zoomButton = createZoomButton(mermaidContainer, content)
+        resetView(zoomButton, mermaidContainer, content)
     });
-}
-
-function onClickZoom(button: HTMLElement, mermaidContainer: HTMLElement, svgContent: string) {
-    if (button.innerText == "Enable Zoom") {
-        enableZoom(button, mermaidContainer)
-    }
-    else {
-        resetView(button, mermaidContainer, svgContent)
-    }
-}
-
-function enableZoom(button: HTMLElement, mermaidContainer: HTMLElement): SvgPanZoom.Instance | null {
-    const svgEl = mermaidContainer.querySelector("svg");
-    if (!svgEl) return null
-
-    const svgSize = svgEl.getBoundingClientRect()
-    svgEl.style.width = svgSize.width+"px";
-    svgEl.style.height = svgSize.height+"px";
-    const panZoomInstance = svgPanZoom(svgEl, {
-        zoomEnabled: true,
-        controlIconsEnabled: true,
-        fit: true,
-        center: true
-    }); 
-    button.innerText = "Disable Zoom"
-    return panZoomInstance
-}
-
-function resetView(button: HTMLElement, mermaidContainer: HTMLElement, svgContent: string) {
-    mermaidContainer.innerHTML = svgContent;
-    mermaidContainer.prepend(button);
-    button.innerText = "Enable Zoom"
 }
 
 window.addEventListener('vscode.markdown.updateContent', init);
