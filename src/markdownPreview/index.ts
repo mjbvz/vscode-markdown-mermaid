4decb0a61a95ef037d4a2530ee1830fa92850282
snopan <snopan@protonmail.com>
2025-01-05 12:39:10 +1100

    Refactor to simplify index.ts, moved panZoomStates inside zoom.ts to manage

--- src/markdownPreview/index.ts
+++ src/markdownPreview/index.ts
@@ -5,16 +5,14 @@
  */
 import mermaid, { MermaidConfig } from 'mermaid';
 import { registerMermaidAddons, renderMermaidBlocksInElement } from '../shared-mermaid';
-import { getToggleButtonStyles, newPanZoomStates, removeOldPanZoomStates, renderZoomableMermaidBlock } from './zoom';
-
-let panZoomStates = newPanZoomStates()
+import { renderMermaidBlocksWithPanZoom } from './zoom';
 
 async function init() { 
     const configSpan = document.getElementById('markdown-mermaid');
     const darkModeTheme = configSpan?.dataset.darkModeTheme;
     const lightModeTheme = configSpan?.dataset.lightModeTheme;
-    const enablePanZoom = configSpan?.dataset.enablePanZoom;
-    const initPanZoom = enablePanZoom ? enablePanZoom == "true" : false
+    const enablePanZoomStr = configSpan?.dataset.enablePanZoom;
+    const enablePanZoom = enablePanZoomStr ? enablePanZoomStr == "true" : false
 
     const config: MermaidConfig = {
         startOnLoad: false,
@@ -26,22 +24,13 @@ async function init() {
     mermaid.initialize(config);
     registerMermaidAddons();
 
-    document.head.appendChild(getToggleButtonStyles())
-    const numElements = await renderMermaidBlocksInElement(document.body, (mermaidContainer, content, index) => {
-        if (initPanZoom) {
-            renderZoomableMermaidBlock(mermaidContainer, content, panZoomStates, index);
-        } else {
-            mermaidContainer.innerHTML = content;
-        }
-    });
 
-    if (initPanZoom) {
-        // Some diagrams maybe removed during edits and if we have states
-        // for more diagrams than there are then we should also remove them
-        removeOldPanZoomStates(panZoomStates, numElements)
+    if (enablePanZoom) {
+        renderMermaidBlocksWithPanZoom()
     } else {
-        // If pan & zoom has been disabled the clear the states completely
-        panZoomStates = newPanZoomStates()
+        renderMermaidBlocksInElement(document.body, (mermaidContainer, content, _) => {
+            mermaidContainer.innerHTML = content;
+        });
     }
 }
 
